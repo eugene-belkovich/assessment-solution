6e3c4a33fb3ef3d3418658d22e57422e934cfe1c
Eugene Belkovich <eugene.belkovich@gmail.com>
2023-03-28 18:28:04 +0200

    chore: - comparator ts warn fixed

--- src/utils/comparator.ts
+++ src/utils/comparator.ts
@@ -2,8 +2,9 @@ import {IGame} from '../entities';
 
 export const sortGamesByTotalAndTimeDesc = (a: IGame, b: IGame) => {
   if (b.totalScore === a.totalScore) {
-    // @ts-ignore
-    return b.startTime - a.startTime;
+    const date1 = new Date(b.startTime).getTime();
+    const date2 = new Date(a.startTime).getTime();
+    return date1 - date2;
   }
   return b.totalScore - a.totalScore;
 };
